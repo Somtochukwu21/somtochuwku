daf9c3a0b7fec67b120b0d382479882364accd4b
Somtochukwu Nnaji <lyon4197@gmail.com>
2024-07-09 13:38:26 +0100

    almost done with the projects

--- app/(landing-page)/_component/footer.tsx
+++ app/(landing-page)/_component/footer.tsx
@@ -1,6 +1,5 @@
 export const Footer = () => (
-	<footer className="bg-secondary text-white p-4 text-center">
+	<footer className="bg-secondary text-white p-4 text-center sticky bottom-0">
 		<p>&copy; 2024 Somtochukwu. All rights reserved.</p>
 	</footer>
 );
-
--- app/(landing-page)/_component/projects.tsx
+++ app/(landing-page)/_component/projects.tsx
@@ -1,5 +1,12 @@
 import { Card } from "@/components/ui";
+import gobet from "@/public/gobet-adminDashboard.png";
+import loom from "@/public/loom-clone1.png";
+import meal from "@/public/meal.png";
+import piabal from "@/public/piabal-createpredictions.png";
+import shortly from "@/public/shortly.png";
+import zigah from "@/public/zigah-dashboard.png";
 import { CardContent, Card as JoyCard } from "@mui/joy";
+import Image, { StaticImageData } from "next/image";
 import React, { ReactNode } from "react";
 
 interface ServiceCardProp {
@@ -39,24 +46,28 @@ export const Projects = () => {
 			</div>
 
 			<div className="mt-4 mb-8">
-				<div className="grid grid-cols-3 grid-rows-5 gap-4 ">
+				<div className="lg:grid grid-cols-3 grid-rows-5 gap-4 ">
 					<div className="row-span-3 [&>*]:h-full">
-						<ProjectCards />
+						<ProjectCards src={zigah} title="Zigah Dashboard" bottom="mb-2" />
 					</div>
 					<div className="row-span-2 ">
-						<ProjectCards />
+						<ProjectCards src={meal} title="meal" display="hidden" />
 					</div>
 					<div className="row-span-3 [&>*]:h-full">
-						<ProjectCards />
+						<ProjectCards
+							src={piabal}
+							title="Piabal Admin Dashboard"
+							bottom="mb-2"
+						/>
 					</div>
 					<div className="row-span-3 [&>*]:h-full col-start-2 row-start-3">
-						<ProjectCards />
+						<ProjectCards src={gobet} title="go bet" bottom="mb-2" />
 					</div>
 					<div className="row-span-2 row-start-4">
-						<ProjectCards />
+						<ProjectCards src={shortly} title="shortly" display="hidden" />
 					</div>
 					<div className="row-span-2 col-start-3 row-start-4">
-						<ProjectCards />
+						<ProjectCards src={loom} title="Loom Clone" display="hidden" />
 					</div>
 				</div>
 			</div>
@@ -64,14 +75,25 @@ export const Projects = () => {
 	);
 };
 
-const ProjectCards = () => {
+interface ProjectCards {
+	src: StaticImageData;
+	display?: string;
+	bottom?: string;
+	title: string;
+}
+const ProjectCards = ({ src, title, display, bottom }: ProjectCards) => {
 	return (
 		<div className="card relative w-full h-[200px] bg-[#f2f2f2] flex items-center justify-center overflow-hidden shadow-[0_0_0_5px_#ffffff80] transition-all duration-[0.6s] ease-[cubic-bezier(0.175,0.885,0.32,1.275)] rounded-[10px] hover:shadow-[0_8px_16px_rgba(255,255,255,0.2)] hover:scale-105">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-				<path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z" />
-			</svg>
+			<div className="[&>*]:w-full w-full h-full">
+				<div className={`${bottom}`}>
+					<Image alt="projects" className="w-full h-full" src={src} />
+				</div>
+				<div className={`${display}`}>
+					<Image alt="projects" className="w-full h-fulll" src={src} />
+				</div>
+			</div>
 			<div className="card__content">
-				<p className="card__title">Card Title</p>
+				<p className="card__title capitalize">{title}</p>
 				<p className="card__description">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
